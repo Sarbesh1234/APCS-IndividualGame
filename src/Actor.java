0d89e6f6b57702b08e3094619ad8ebac4c14f6a5
sarbesh <sankarsarbesh@gmail.com>
2021-05-02 14:22:06 -0700

    Implemented actor class methods

--- src/Actor.java
+++ src/Actor.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
 
 public abstract class Actor extends javafx.scene.image.ImageView {
 
@@ -7,6 +11,44 @@ public abstract class Actor extends javafx.scene.image.ImageView {
 	
 	public abstract void act(long now);
 	
+	public double getHeight() {
+		return getBoundsInParent().getHeight();
+	}
+	
+	public double getWidth() {
+		return getBoundsInParent().getWidth();
+	}
+	
+	public World getWorld() {
+		return (World) getParent();
+	}
+	
+	public void move(double dx, double dy) {
+		this.setX(getX()+dx);
+		this.setY(getY()+dy);
+	}
+	
+	public <A extends Actor> java.util.List<A> getIntersectingObjects(java.lang.Class<A> cls) {
+		ArrayList<A> arr = new ArrayList<A>();
+		ObservableList<Node> nodesList = getWorld().getChildren();
+		for(Node node: nodesList) {
+			if(cls.isInstance(node) && this != node && node.intersects(this.getBoundsInParent())){
+				arr.add((A) node);
+			}
+		}
+		return arr;
+		
+	}
+
+	public <A extends Actor> A getOneIntersectingObject(java.lang.Class<A> cls) {
+		ObservableList<Node> nodesList = getWorld().getChildren();
+		for(Node node: nodesList) {
+			if(cls.isInstance(node) && this != node && node.intersects(this.getBoundsInParent())){
+				return (A) node;
+			}
+		}
+		return null;
+	}
 	
 
 }
